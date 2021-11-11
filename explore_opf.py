55aef10b31158d89e1dc5e69978094913ae85790
Zhenya <e.kalenkovich@gmail.com>
2021-11-11 16:23:41 -0500

    first pass at checking all the pho's

--- explore_opf.py
+++ explore_opf.py
@@ -53,5 +53,54 @@ class OPFFile(object):
         return df
 
 
+def collect_all_chi(opf: OPFFile):
+    df = opf.to_pandas_df()
+
+    # Find child utterances
+    is_chi = df.speaker == 'CHI'
+
+    # Find phonetic transcriptions in separate cells
+    is_pho = df.object.str.startswith('%pho')
+    assert (df[is_pho].time_start == df[is_pho].time_end).all()
+
+    # Merge
+    # The only fields in %pho rows we care about are time_end, annotid and object. Others are either empty or redundant
+    # (time_start == time_end is True)
+    chi_with_pho = df[is_chi].merge(
+        df[is_pho][['object', 'time_end']],
+        on='time_end',
+        suffixes=('', '_pho'),
+        how='outer',
+        # Check for non-one-to-one matches
+        validate='1:1')
+
+    # Check for orphan %pho's
+    assert chi_with_pho.object.isna().sum() == 0
+
+    # # Add flags from the table above
+
+    # Is there a pho cell?
+    chi_with_pho['is_pho_cell'] = ~chi_with_pho.object_pho.isna()
+
+    # Does it have anything in it?
+    # First, check that they all have the same prefix "%pho: "
+    PHO_PREFIX = '%pho: '
+    assert chi_with_pho.object_pho.str.startswith(PHO_PREFIX).all()
+    # Is there at least one character after the prefix?
+    chi_with_pho['is_pho_cell_filled'] = chi_with_pho.object_pho.str[len(PHO_PREFIX):].str.strip().str.len() > 0
+
+    chi_with_pho['is_pho_field'] = 'pho' in chi_with_pho.columns
+
+    chi_with_pho['is_pho_field_filled'] = chi_with_pho.pho.str.strip().str.len() > 0
+
+    return chi_with_pho
+
+
 opf_path = Path('01_17_sparse_code.opf')
 opf = OPFFile(opf_path)
+chi_with_pho = collect_all_chi(opf)
+
+# Make a pivot table
+print(chi_with_pho
+      .groupby(['is_pho_cell', 'is_pho_cell_filled', 'is_pho_field', 'is_pho_field_filled'])
+      .size())
