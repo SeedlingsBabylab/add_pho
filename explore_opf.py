52e3fade2d75423a32b3b2ae7383bd5fd2d437dd
Zhenya <e.kalenkovich@gmail.com>
2021-11-19 10:10:58 -0500

    merge across files early

--- explore_opf.py
+++ explore_opf.py
@@ -61,40 +61,6 @@ def collect_all_chi(opf: OPFDataFrame):
     return chis_with_phos
 
 
-def add_flags(chis_with_phos):
-    """
-    Adds binary columns 'is_pho_cell', 'is_pho_cell_filled', 'is_pho_field', 'pho', 'is_pho_field_filled'
-    :param chis_with_phos: output of collect_all_chi
-    :return: chis_with_phos with four additional columns.
-    """
-
-    # Is there a pho cell?
-    chis_with_phos['is_pho_cell'] = ~chis_with_phos.object_pho.isna()
-
-    # Does it have anything in it?
-    # First, check that they all have the same prefix "%pho: "
-    assert chis_with_phos.object_pho.str.contains(PHO_PREFIX).all()
-    # Is there at least one character after the prefix?
-    chis_with_phos['is_pho_cell_filled'] = chis_with_phos.object_pho.str.replace(PHO_PREFIX, '', regex=True).str.len() > 0
-
-    # Is there a pho field
-    is_pho_field = 'pho' in chis_with_phos.columns
-    chis_with_phos['is_pho_field'] = is_pho_field
-
-    # Does it have anything in it?
-    # First, check that they all have the same prefix "%pho"
-    assert chis_with_phos.object_pho.str.contains(PHO_PREFIX).all()
-    # Is there at least one character after the prefix?
-    if is_pho_field:
-        chis_with_phos['is_pho_field_filled'] = chis_with_phos.pho.str.replace(PHO_PREFIX, '', regex=True).str.len() > 0
-    # If there was no pho field, add a NaN column. This is different from an empty pho field which is an empty string.
-    else:
-        chis_with_phos['pho'] = np.nan
-        chis_with_phos['is_pho_field_filled'] = np.nan
-
-    return chis_with_phos
-
-
 # # Main
 
 # locate all the opf files
@@ -103,21 +69,24 @@ opf_paths = find_matching(list_of_roots=[subject_files_dir], pattern=opf_path_pa
 
 # Read and convert to dataframes
 opf_files = list(map(OPFFile, opf_paths))
-opf_df = list(map(OPFDataFrame, opf_files))
+opf_dfs = list(map(OPFDataFrame, opf_files))
 
 # Find all the CHIs, the corresponding phos, and classify them
-all_chis_with_phos = list(map(collect_all_chi, opf_df))
+all_chis_with_phos = pd.concat(
+    objs=map(collect_all_chi, opf_dfs),
+    keys=[opf.path for opf in opf_files],
+    names=['file_path', 'index']
+).reset_index(0)
 
 
 # # Find all the orphan phos
-orphans = pd.concat(objs=[df[df.object.isna()] for df in all_chis_with_phos],
-                    keys=[opf.path for opf in opf_files],
-                    names=['file_path', 'index'])
+orphans = all_chis_with_phos[all_chis_with_phos.object.isna()].copy()
 
-# Clean and save to a csv
+# Clean
 # A random date was added to time for technical reasons, we don't need it anymore
-orphans.time_end = orphans.time_end.dt.time
-orphans = orphans[['object_pho', 'id_pho', 'time_start_pho', 'time_end_pho']].reset_index(0)
+orphans.time_end_pho = orphans.time_end_pho.dt.time
+orphans = orphans[['file_path', 'object_pho', 'id_pho', 'time_start_pho', 'time_end_pho']]
+
 
 # Save
 orphans_output_path = Path('repo') / 'reports' / 'orphan_phos.csv'
@@ -141,30 +110,82 @@ duplicates = pd.concat(
     keys=[opf.path for opf in opf_files],
     names=['file_path', 'type of duplicate', 'index'])
 
+
+# These column combinations are supposed to be unique
+unique_chi_columns = ['file_path', 'id', 'time_start', 'time_end']
+unique_pho_columns = ['file_path', 'id_pho', 'time_start_pho', 'time_end_pho']
+
+duplicates = pd.concat(
+        objs=[all_chis_with_phos[(
+                all_chis_with_phos.duplicated(subset=unique_chi_columns, keep=False)
+                & ~all_chis_with_phos.object.isna())],  # don't count empty rows as duplicates of each other
+              all_chis_with_phos[(
+                all_chis_with_phos.duplicated(subset=unique_pho_columns, keep=False)
+                & ~all_chis_with_phos.object_pho.isna())]],
+        keys=['CHIs sharing a %pho', '%phos sharing a CHI'],
+        names=['duplicate_type', 'index']
+    ).reset_index(0)
+
 duplicates_output_path = Path('repo') / 'reports' / 'duplicates.csv'
 duplicates_output_path.parent.mkdir(exist_ok=True)
-duplicates.reset_index([0, 1]).to_csv(duplicates_output_path, index=False)
+duplicates.to_csv(duplicates_output_path, index=False)
 
 
-# Add the flags and make summary tables
-all_chis_with_phos_with_flags = list(map(add_flags, all_chis_with_phos))
+# Classify based on pho field/cell presence and the transcription actually being there
+def add_flags(chis_with_phos):
+    """
+    Adds binary columns 'is_pho_cell', 'is_pho_cell_filled', 'is_pho_field', 'pho', 'is_pho_field_filled'
+    :param chis_with_phos: output of collect_all_chi
+    :return: chis_with_phos with four additional columns.
+    """
+
+    # Is there a pho cell?
+    chis_with_phos['is_pho_cell'] = ~chis_with_phos.object_pho.isna()
+
+    # Does it have anything in it?
+    # First, check that they all have the same prefix "%pho: " (all() does not care about NaNs)
+    assert chis_with_phos.object_pho.str.contains(PHO_PREFIX).all()
+    # Is there at least one character after the prefix?
+    chis_with_phos['is_pho_cell_filled'] = (
+        (chis_with_phos.object_pho
+         # Remove the prefix
+         .str.replace(PHO_PREFIX, '', regex=True)
+         # Is there anything left?
+         .str.len() > 0)
+        # We want NaN, not False when there was no pho field
+        .where(chis_with_phos.is_pho_cell)
+    )
+
+    # Is there a pho field?
+    # If there was, and it was empty, then it would equal to '' now; if there wasn't, it would now be NaN.
+    chis_with_phos['is_pho_field'] = ~chis_with_phos.pho.isna()
+
+    # Does it have anything in it?
+    # First, check that they all have the same prefix "%pho: "
+    assert chis_with_phos.object_pho.str.contains(PHO_PREFIX).all()
+    # Is there at least one character after the prefix?
+    chis_with_phos['is_pho_field_filled'] = (
+        (chis_with_phos.pho
+         # Remove the prefix
+         .str.replace(PHO_PREFIX, '', regex=True)
+         # Is there anything left?
+         .str.len() > 0)
+        # We want NaN, not False when there was no pho field
+        .where(chis_with_phos.is_pho_field))
+
+    return chis_with_phos
 
 
+all_chis_with_phos_with_flags = add_flags(all_chis_with_phos)
+
+
+# Make summary tables
 def make_pivot(chis):
     return (chis
-            .groupby(['is_pho_cell', 'is_pho_cell_filled', 'is_pho_field', 'is_pho_field_filled'], dropna=False)
+            .groupby(['is_pho_cell', 'is_pho_cell_filled', 'is_pho_field', 'is_pho_field_filled', 'is_orphan'], dropna=False)
             .size()
             .to_frame('size')
             .reset_index())
 
 
-pivots = pd.concat(
-    objs=map(make_pivot, all_chis_with_phos),
-    keys=[opf.path for opf in opf_files],
-    names=['file_path', 'index']
-)
-
-grand_pivot = (pivots
-               .groupby(['is_pho_cell', 'is_pho_cell_filled', 'is_pho_field', 'is_pho_field_filled'], dropna=False)
-               .sum()
-               .reset_index())
+make_pivot(all_chis_with_phos_with_flags)
