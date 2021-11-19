1d0bed7fb8f61d86fad5ba03b1e2e3f2399361ef
Zhenya <e.kalenkovich@gmail.com>
2021-11-19 09:42:20 -0500

    factor out finding files

--- explore_opf.py
+++ explore_opf.py
@@ -1,13 +1,11 @@
 import datetime
 from pathlib import Path
-import os
-import re
 
 import pandas as pd
 import numpy as np
 
-
 from opf import OPFFile, OPFDataFrame
+from seedlings import subject_files_dir, find_matching
 
 
 PHO_PREFIX = r'^%pho:?(?:&|\s+)'
@@ -87,42 +85,7 @@ def add_flags(chis_with_phos):
     return chis_with_phos
 
 
-# # Find all opfs
 
-# Locate Subject_Files
-pn_opus_dir = Path(os.environ.get('pn_opus') or '/Volumes/pn-opus')
-assert pn_opus_dir.exists(), f'Mount pn-opus or set env var "pn_opus" to actual path instead of {pn_opus_dir}'
-subject_files_dir = pn_opus_dir / 'Seedlings' / 'Subject_Files'
-
-
-# Function to find files somewhat efficiently
-def find_matching(list_of_roots, pattern):
-    # Recursion stop
-    if pattern is None:
-        return list_of_roots
-
-    if '/' in pattern:
-        this_level, rest_of_the_pattern = pattern.split('/', maxsplit=1)
-    else:
-        this_level, rest_of_the_pattern = pattern, None
-
-    new_roots = [match
-                 for root in list_of_roots
-                 for match in filter(lambda x: re.match(this_level, x.name),
-                                     root.glob('*'))]
-
-    # list_of_roots = new_roots[:1]
-    # pattern = rest_of_the_pattern
-
-    return find_matching(new_roots, rest_of_the_pattern)
-
-
-# Find all opf files
-opf_path = r'\d{2}/\d{2}_\d{2}/Home_Visit/Coding/Video_Annotation/\d{2}_\d{2}_sparse_code.opf'
-
-list_of_roots = [subject_files_dir]
-pattern = opf_path
-opf_paths = find_matching(list_of_roots, pattern)
 
 opf_files = list(map(OPFFile, opf_paths))
 opf_df = list(map(OPFDataFrame, opf_files))
