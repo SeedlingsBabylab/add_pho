f1283c894feb1a58388e867b034f015def9617aa
Zhenya <e.kalenkovich@gmail.com>
2021-12-02 16:20:11 -0500

    batch_parse_clan2 - correct input
    It needs a path to a txt file that contains paths to folders that contain cha files.

--- run_batch_parse_clan2.sh
+++ run_batch_parse_clan2.sh
@@ -1,5 +1,5 @@
 script=/Volumes/pn-opus/Seedlings/Scripts_and_Apps/Github/seedlings/parse_clan2/batch_parse_clan2.py
-cha_files_master=/Volumes/pn-opus/Seedlings/Scripts_and_Apps/Github/seedlings/annotated_cha/annotated_cha/
+cha_file_paths=/Volumes/pn-opus/Seedlings/Scripts_and_Apps/Github/seedlings/path_files/cha_sparse_code_paths.txt
 processed_files=/Users/ek221/blab/02_fix-pho/repo
 
 cha_files=$cha_files_master
