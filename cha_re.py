7405a5652c94373707bc72b4a6b17a4c3da24108
Zhenya <e.kalenkovich@gmail.com>
2021-12-02 11:30:10 -0500

    relax regexes a bit more

--- cha_re.py
+++ cha_re.py
@@ -1,12 +1,13 @@
-speaker = '(?P<speaker>CHI|MOT|FAT|SIS|AUN|TOY|MCU|BRO|GRA|SI1|MT2|FCO|BSJ|GRM)'
-annotation = fr'(?P<word>[\w+]+) &=[s|d|n|y|i|q|r]_[n|y|u]_{speaker}_0x[a-z0-9]{{6}}'
+speaker = '(?P<speaker>CHI|MOT|FAT|SIS|AUN|TOY|MCU|BRO|GRA|SI1|SI2|MT2|FCO|BSJ|GRM|GP2|BR1|GRP|MGM|AFD)'
+annotation = fr'(?P<word>[\w+]+) +&=[s|d|n|y|i|q|r]_[n|y|u]_{speaker}_0x[a-z0-9]{{6}}'
 annotations = fr'(?P<annotations>(?:{annotation} +)*)'
 
 lena_annotation = r'(?:0|&=(?:w\d+(?:_\d+)?|vocalization|crying|vfx))'
 timestamp = r'\x15\d+_\d+\x15'
 maybe_zero = r'(?:0 +)?'
 maybe_dot = r'(?:\. +)?'
-other = fr'(?:{lena_annotation} +)?{maybe_zero}{maybe_dot}{timestamp}'
+maybe_zero_and_a_dot = r'(?:0\. +)?'
+other = fr'(?:{lena_annotation} +)?{maybe_zero_and_a_dot}{maybe_zero}{maybe_dot}{timestamp}'
 
 main_tier_content_pattern = fr'{annotations}{other}$'
 # used to identify main tier lines manually split over multiple lines
