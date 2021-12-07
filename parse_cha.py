7884f38c640f3af28379a30c9bb8110808de4239
Zhenya <e.kalenkovich@gmail.com>
2021-12-07 11:46:30 -0500

    clean code

--- parse_cha.py
+++ parse_cha.py
@@ -1,30 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[0]
-from contextlib import redirect_stdout
+from pathlib import Path
+from collections import defaultdict
 
 from IPython import get_ipython
 
 get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '1')
 get_ipython().run_line_magic('aimport', 'cha')
-get_ipython().run_line_magic('aimport', 'cha_re')
 from cha import CHAFile
-
-# In[]
-from pathlib import Path
-
-
-
-# In[2]:
 
 
 SPEAKER_CODE = 'CHI'
 
 
-# In[3]:
-
+# In[]:
+# # Find all the chas
 seedlings_path = Path('/Volumes/pn-opus/Seedlings')
 assert seedlings_path.exists()
 
@@ -35,8 +24,8 @@ with cha_file_path_list.open('r', encoding='utf-8') as f:
     cha_paths = list(map(lambda line: Path(line.rstrip()), f))
 
 
+# In[]:
 # # Load an parse
-# In[4]:
 
 cha_files = [CHAFile(cha_path) for cha_path in cha_paths]
 
@@ -47,9 +36,7 @@ for i, cha_file in enumerate(cha_files):
     cha_file.process_for_phonetic_transcription(SPEAKER_CODE)
 
 
-# # Step 1, check for errors
-
-# Three main tiers have been updated and now have annotid missing. We will skip them here.
+# Check for parsing errors
 first_lines_to_skip = (
     'penguin &=n_y_CHI_0x352e93 penguin &=n_y_CHI_0x366d19 penguin\n',
     'oranges &=d_y_MOT_0x1d8f75 apples &=d_n_MOT_0x4945d6 oranges\n',
@@ -57,13 +44,10 @@ first_lines_to_skip = (
 
 assert not any(mt.errors and mt.contents[0] not in first_lines_to_skip for cf in cha_files for mt in cf.main_tiers)
 
-# # Step 2
-# 
-# Manually check the ones where the number of annotated words is different from the number of the transcriptions.
 
-# In[60]:
-
-from collections import defaultdict
+# In[]
+# # Add
+# Check for transcription errors (too few, too many, etc.)
 
 main_tiers = defaultdict(list)
 for cf in cha_files:
@@ -73,10 +57,6 @@ for cf in cha_files:
             continue
         main_tiers[mt.update_pho(SPEAKER_CODE)].append((cf, mt))
 
-for status, mts in main_tiers.items():
-    print(status, len(mts))
-
-# After manual edits, the only error should be "error: CHI not in annotation"
 assert not any(status.startswith('error') for status in main_tiers)
 
 
